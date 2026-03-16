e35ca6a719ac518f520d5171d3dd19f883b0e837
Eduard Leicht <eduard.leicht.87@gmail.com>
2026-03-16 12:42:17 +0100

    Erster Stand: Maven Setup und Raum-Klasse fertig

---
+++ src/main/java/de/eddie/cube/Main.java
@@ -0,0 +1,9 @@
+package de.eddie.cube;
+
+public class Main{
+    public static void main( String[] args ){
+        int[] test = {123, 456, 789};
+
+
+    }
+}
---
+++ src/main/java/de/eddie/cube/Room.java
@@ -0,0 +1,14 @@
+package de.eddie.cube;
+
+public class Room{
+    private int[] roomNumber;
+    private int x, y, z;
+    private boolean aExit;
+
+    public Room (int[] roomNumber, int x, int y, int z){
+        this.roomNumber = roomNumber;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
